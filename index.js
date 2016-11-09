3875e4278a34b96e5a0bbf5d72b56ff54d195cd7
druide <ingvuil@gmail.com>
2016-11-09 14:37:48 +0200

    [Fix] require error

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('flow-remove-types/register')
+require('@druide/flow-remove-types-register')
 
 module.exports = require('./lib/rate-limiter')
 module.exports.TokenBucket = require('./lib/token-bucket')
